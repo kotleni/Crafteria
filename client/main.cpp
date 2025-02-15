26d66e9b3ba7c2159eed218eda1fcad7d149f67c
Viktor Varenik <yavarenikya@gmail.com>
2025-02-15 23:48:34 +0200

    chore: disable linear filtering for textures

--- client/main.cpp
+++ client/main.cpp
@@ -507,8 +507,8 @@ int main() {
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     }
     stbi_image_free(cobblestoneImage->raw);
     delete cobblestoneImage;
